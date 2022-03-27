b4c7181f0b2ef154c52e3818b2faae36cb483d43
MRiding-ANU <91646974+MRiding-ANU@users.noreply.github.com>
2022-03-28 09:04:31 +1100

    Adjust to run on AATLXY
    Minor changes so that plot diff will run on AATLXY.

--- plotdiff.py
+++ plotdiff.py
@@ -7,6 +7,13 @@
 # Option to exclude the offset
 # 2020/02/14
 # Prints out the offset
+# 2022/03/28
+# MR adapting to run on aatlxy
+
+# To run code on AATLXY, go to /instsoft/2dF/config/poscheck_diff/
+# Then run below command,
+# python plotdiff.py -a -1 /path/to/old/sdsraw.sds -2 /path/to/new/sdsraw.sds
+# PDF document will be saved in /instsoft/2dF/config/poscheck_diff/
 
 import numpy
 import matplotlib.pyplot as plt
@@ -25,12 +32,12 @@ class nf(float):
 class poscheck:
     """A poscheck result"""
     def __inti__(self):
-        self.c0=0.0              
-        self.c1=0.0              
+        self.c0=0.0
+        self.c1=0.0
         self.c2=0.0
-        self.c3=0.0        
-        self.c4=0.0              
-        self.c5=0.0        
+        self.c3=0.0
+        self.c4=0.0
+        self.c5=0.0
         self.x_off=0.0        # microns
         self.y_off=0.0        # microns
         self.a=13366713.7     # Number of microns per radian
@@ -48,7 +55,7 @@ class poscheck:
         # Distortion correction first
         x1=(x-self.x_off/self.a)
         y1=(y-self.y_off/self.a)
-        
+
         # self.x_off and self.y_off in microns
         # x1 and y1 are in radians
         # x and y are in radians
@@ -65,12 +72,12 @@ class poscheck:
 
         x_3=self.c0+self.c1*x2+self.c2*y2
         y_3=self.c3+self.c4*x2+self.c5*y2
-        
+
         # x_3 and y_3 are in microns
 
         # Convert from radians to arc seconds
         factor = self.a / (3600 * 180 / numpy.pi)
-        
+
         return x_3/factor,y_3/factor
 
 parser = OptionParser()
@@ -90,22 +97,27 @@ parser.add_option("-s", "--scale", dest="scale", default=0.00005, type=float,
 parser.add_option("-l", "--length", dest="length", default=1.0, type=float,
                   help="Length of arrow in arc seconds")
 
-parser.add_option("-a", "--applyOffset", dest="applyOffset", default=False, 
+parser.add_option("-a", "--applyOffset", dest="applyOffset", default=False,
                   action="store_true",
                   help="Apply offset")
 
 
 (options, args) = parser.parse_args()
 
+# Add path for sdsdump. Shells do not have drama commands loaded.
+# Done in a hack method for ease as this script should not be used elsewhere.
+sdspath = '/instsoft/drama/release/sds/r1_4_3_92/linux_x86/'
 
 p1=poscheck()
 p2=poscheck()
 
 cmd="sdsdump %s PosCheckResults.newDist" % (options.file1)
+cmd = sdspath + cmd
 p = sp.Popen(cmd,stdout=sp.PIPE,shell=True)
 output = p.communicate()[0].split()
 
 cmd="sdsdump %s PosCheckResults.newCoeffs" % (options.file1)
+cmd = sdspath + cmd
 p = sp.Popen(cmd,stdout=sp.PIPE,shell=True)
 output2 = p.communicate()[0].split()
 
@@ -117,16 +129,18 @@ p1.c4=float(output2[4])
 p1.c5=float(output2[5])
 p1.x_off=float(output[4])
 p1.y_off=float(output[5])
-p1.a=13366713.699999999255  #1e6  
+p1.a=13366713.699999999255  #1e6
 p1.b=1089450000.0           #1e12
 p1.c=621000000000.0         #1e18
 p1.d=331200000000000.0      #1e24
 
 cmd="sdsdump %s PosCheckResults.newDist" % (options.file2)
+cmd = sdspath + cmd
 p = sp.Popen(cmd,stdout=sp.PIPE,shell=True)
 output = p.communicate()[0].split()
 
 cmd="sdsdump %s PosCheckResults.newCoeffs" % (options.file2)
+cmd = sdspath + cmd
 p = sp.Popen(cmd,stdout=sp.PIPE,shell=True)
 output2 = p.communicate()[0].split()
 
@@ -164,7 +178,7 @@ length=244957.8
 
 offset=numpy.sqrt((x_offset)**2.+(y_offset)**2.)
 
-# Compute the average over the middle 
+# Compute the average over the middle
 selection =(xx*xx+yy*yy < (length  * 0.6667)**2.)
 
 fig=plt.figure(figsize=(10,10))
@@ -179,7 +193,9 @@ else:
     levels = numpy.arange(-2, 2.1, 0.1) + numpy.average(offset[selection])
     CS=ax.contour(xx,yy,offset,levels)
 
-print('Average Offset %4.1f %4.1f (arc seconds)' % (numpy.average(x_offset[selection]),numpy.average(y_offset[selection])))
+avg_offset = 'Average Offset %4.1f %4.1f (arc seconds)' % (numpy.average(x_offset[selection]),numpy.average(y_offset[selection]))
+
+print(avg_offset)
 
 if options.applyOffset:
     q = ax.quiver(xx, yy, x_offset-numpy.average(x_offset[selection]), y_offset-numpy.average(y_offset[selection]),scale_units='xy',scale=options.scale)
@@ -214,7 +230,8 @@ ax.add_artist(circle1)
 ax.set_xlim(-1*length,length)
 ax.set_ylim(-1*length,length)
 
-plt.savefig("%s_%s.pdf" % (options.file1.replace(".sds","").replace("/","_"),options.file2.replace(".sds","").replace("/","_")))
+ax.set_title(avg_offset)
+
+plt.savefig("%s_%s.pdf" % (options.file1.replace(".sds","").replace("../","").replace("/","_"),options.file2.replace(".sds","").replace("../","").replace("/","_")))
 plt.show()
 plt.close()
-
